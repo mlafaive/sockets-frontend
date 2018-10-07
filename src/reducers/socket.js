ffb0224b52a57140254fd08bba814552d62c8553
mlafaive <mlafaive@umich.edu>
2018-10-07 13:18:00 -0700

    pointed at prod server

--- src/reducers/socket.js
+++ src/reducers/socket.js
@@ -1,16 +1,16 @@
 export function socket(state = null, action) {
   switch (action.type) {
   	case 'CREATE_SOCKET':
-      return window.socketCluster.connect({
-        hostname: 'localhost',
-        secure: false,
-        port: 8000
-      });
       // return window.socketCluster.connect({
-      //   hostname: 'sockets-app.herokuapp.com',
-      //   secure: true,
-      //   port: 443
+      //   hostname: 'localhost',
+      //   secure: false,
+      //   port: 8000
       // });
+      return window.socketCluster.connect({
+        hostname: 'sockets-app.herokuapp.com',
+        secure: true,
+        port: 443
+      });
     case 'CLEAR_SOCKET':
       state.destroy();
       return null;
